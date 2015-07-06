33b5399f039d250813bf01d91ceae20c464a88f3
Maciej Dziardziel <fiedzia@gmail.com>
2015-07-06 21:33:40 +0100

    imake it work with python3

--- open_facebook/api.py
+++ open_facebook/api.py
@@ -83,6 +83,7 @@ understand the required functionality
 
 from django.http import QueryDict
 from django.utils import six
+from django.utils.encoding import smart_str
 from django.utils.http import urlencode
 from django_facebook import settings as facebook_settings
 from open_facebook import exceptions as facebook_exceptions
@@ -472,7 +473,7 @@ class FacebookAuthorization(FacebookConnection):
         sig = base64_url_decode_php_style(encoded_sig)
         import hmac
         import hashlib
-        data = json.loads(base64_url_decode_php_style(payload))
+        data = json.loads(smart_str(base64_url_decode_php_style(payload)))
 
         algo = data.get('algorithm').upper()
         if algo != 'HMAC-SHA256':
@@ -482,7 +483,7 @@ class FacebookAuthorization(FacebookConnection):
             logger.error('Unknown algorithm')
             return None
         else:
-            expected_sig = hmac.new(secret, msg=payload,
+            expected_sig = hmac.new(six.binary_type(secret, 'utf8'), msg=six.binary_type(payload, 'utf8'),
                                     digestmod=hashlib.sha256).digest()
 
         if sig != expected_sig:
